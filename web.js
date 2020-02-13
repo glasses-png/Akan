cbbd0793b912e7c6ea5d1b5094fe8c5587860d89
glasses-png <cchelangat16@gmail.com>
2020-02-13 10:27:55 +0300

    add variables

--- web.js
+++ web.js
@@ -1,4 +1,12 @@
 function output(){
   var day=document.getElementById("day").nodeValue;
-
+  var dd=parseInt(day);
+  var month=document.getElementById("month").nodeValue;
+  var mm=parseInt(month);
+  var year=document.getElementById("year").nodeValue;
+  var yy=parseInt(year);
+  var cc=(yy-1)/100+1;
+  var result=parseInt(((cc/4)-2*cc-1)+((5*yy/4))+((26*(mm+1)/10))+dd)%7;
+  var boy=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+  var girl=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 }
